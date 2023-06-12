73d13fe83880f11603a04a220878abd49596d7a0
SrZitu <zitu094@gmail.com>
2023-06-12 16:53:45 +0600

    assignment__17

--- assignment_17/routes/web.php
+++ assignment_17/routes/web.php
@@ -18,4 +18,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/post',[PostController::class,'index']);
+Route::get('/excerptDescription', [PostController::class, 'excerptDescription']);
+
+Route::get('/distinctSelect', [PostController::class, 'distinctSelect']);
+
+Route::get('/retriveFirst', [PostController::class, 'retriveFirst']);
+
+Route::get('/description', [PostController::class, 'description']);
+
+Route::get('/title', [PostController::class, 'title']);
+
+Route::post('/insertData', [PostController::class, 'insertData']);
+
+Route::patch('/update ', [PostController::class, 'update']);
+
+Route::delete('/delete  ', [PostController::class, 'delete']);
+
+Route::get('/minData  ', [PostController::class, 'minData']);
+
+Route::get('/incrementByOne  ', [PostController::class, 'incrementByOne']);
